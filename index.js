d69fd6fc064577e8d41062516fc748cf42b77399
peterpadilla6803 <82825024+peterpadilla6803@users.noreply.github.com>
2021-05-08 20:14:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ const client = new Discord.Client()
 
 const fs = require('fs')
     
-// Create the empty commands object
 const commands = {}
-// Get the file names from the commands directory
 const files = fs.readdirSync('./commands')
-// Filter out any non-js files
 const jsFiles = files.filter(file => file.endsWith('.js'))
-// Foreach, require the file, check for the right exports, then add to the commands object
 jsFiles.forEach(commandFile => {
   const command = require(`./commands/${commandFile}`)
   if (command.prefix && command.fn) {
@@ -29,13 +25,10 @@ client.on('message', async msg => {
     if(msg.author.bot){
         return
     }
-    // Grab the prefix, which should be the content before the first space
     const prefix = msg.content.split(' ')[0];
-    // Filter out bad commands and bots
     if (commands[prefix] === undefined || msg.author.bot) {
       return
     }
-    // Execute the fn of the prefix object, passing in the message
     commands[prefix](msg);
   })
 
